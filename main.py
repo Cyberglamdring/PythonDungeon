c4ca0f0531bc5056b6af7965d00ec924bcd5d92d
Hleb Kanonik <50049166+Cyberglamdring@users.noreply.github.com>
2019-09-17 09:54:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,11 +10,12 @@ required = "\nUse only A, B, or C\n"
 
 stop = time.sleep
 
+# health = 100
+# attack = 0
+# weapon = "empty"
 
-def status():
-    attack = 0
-    health = 100
-    weapon = "empty"
+
+def status(health, attack, weapon):
     print(f"\n===========\nHERO STATUS\n==========="
           f"\n♥ {health}"
           f"\n⚔ {attack}"
@@ -26,10 +27,10 @@ def intro():
     # name
     name = input("What is your name, adventurer?\n")
     print(f"Greetings, {name}. Let us go on a quest!"
-          "\nYou find yourself on the edge of a dark forest")
+          "\nYou find yourself on the edge of a Dark Forest!")
     stop(1)
     # status
-    status()
+    status(100, 0, "empty")
 
     # first step
     stop(1)
@@ -38,7 +39,7 @@ def intro():
     print(text)
 
     # first choice
-    stop(3)
+    stop(2)
     ia = open('texts/intro_answer', 'r')
     text = ia.read()
     print(text)
@@ -48,11 +49,7 @@ def intro():
         option_rock()
     elif choice in answer_B:
         print("\nWell, that was quick.\n\n")
-
-        death = open('texts/death', 'r')
-        text = death.read()
-        print(text)
-
+        option_death()
     elif choice in answer_C:
         option_run()
     else:
@@ -60,12 +57,45 @@ def intro():
         intro()
 
 
+def option_death():
+    status(0, 0, "empty")
+
+    death = open('texts/death', 'r')
+    text = death.read()
+    print(text)
+
+
 def option_rock():
-    print("step option_rock")
+    rt = open('texts/rock', 'r')
+    text = rt.read()
+    print(text)
+    stop(1)
+
+    rt = open('texts/rock_answer', 'r')
+    text = rt.read()
+    print(text)
+    stop(1)
+    choice = input(">>> ")
+    if choice in answer_A:
+        option_run()
+    elif choice in answer_B:
+        print("\nYou decided to throw another rock, as if the first "
+              "rock thrown did much damage. The rock flew well over the "
+              "orcs head. You missed.")
+        option_death()
+    elif choice in answer_C:
+        option_cave()
+    else:
+        print(required)
+        option_rock()
 
 
 def option_run():
     print("step option_run")
+
+
+def option_cave():
+    print("step option_cave")
 
 
 intro()
